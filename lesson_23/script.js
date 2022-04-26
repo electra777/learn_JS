ea126fe8a97d64c3caed7112a7291ec41e345766
electra777 <electra0789@gmail.com>
2022-04-26 21:27:55 +0300

    add new tasks

--- lesson_23/script.js
+++ lesson_23/script.js
@@ -18,3 +18,74 @@ for (let i = 1; i < length; i++) {
 }
 
 console.log(result);
+
+// Место для первой задачи
+function firstTask() {
+	// Пишем решение вот тут
+	for (let i = 5; i <= 10; i++) {
+		console.log(i);
+	}
+}
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+	// Пишем решение вот тут
+	for (let i = 20; i >= 10; i--) {
+		if (i === 13) break;
+		console.log(i);
+	}
+}
+secondTask();
+
+// Место для третьей задачи
+function thirdTask() {
+	// Пишем решение вот тут
+	for (let i = 2; i <= 10; i++) {
+		if (i % 2 === 0) {
+			console.log(i);
+		}
+	}
+}
+thirdTask();
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+	// Пишем решение вот тут
+	let i = 2;
+	while (i < 16) {
+		i++;
+		if (i % 2 === 0) {
+			continue;
+		} else {
+			console.log(i);
+		}
+	}
+}
+fourthTask();
+
+// Место для пятой задачи
+
+function fifthTask() {
+	const arrayOfNumbers = [];
+
+	// Пишем решение вот тут
+	for (let i = 5; i <= 10; i++) {
+		arrayOfNumbers.push(i);
+	}
+
+	// Не трогаем
+	return arrayOfNumbers;
+}
+fifthTask();
