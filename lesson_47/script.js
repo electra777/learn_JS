8fe6e933a50b6b3cb78c9a33fa72bae1576c0c07
electra777 <electra0789@gmail.com>
2022-06-06 19:13:57 +0300

    add lesson 47 (function pow)

---
+++ lesson_47/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function pow(x, n) {
+	let result = 1;
+	for (let i = 0; i < n; i++) {
+		result *= x;
+	}
+	return result;
+}
+
+console.log(pow(2, 2)); // 4
+console.log(pow(2, 3)); // 8
+console.log(pow(2, 4)); // 16
