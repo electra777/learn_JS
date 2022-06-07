9af5511165204a1f9c213609753349ac1250cc3b
electra777 <electra0789@gmail.com>
2022-06-07 20:01:40 +0300

    add function recursion

--- lesson_47/script.js
+++ lesson_47/script.js
@@ -1,13 +1,28 @@
 'use strict';
 
+// function pow(x, n) {
+// 	let result = 1;
+// 	for (let i = 0; i < n; i++) {
+// 		result *= x;
+// 	}
+// 	return result;
+// }
+
+// console.log(pow(2, 0)); // 1
+// console.log(pow(2, 1)); // 2
+// console.log(pow(2, 2)); // 4
+// console.log(pow(2, 3)); // 8
+// console.log(pow(2, 4)); // 16
+
 function pow(x, n) {
-	let result = 1;
-	for (let i = 0; i < n; i++) {
-		result *= x;
+	if (n === 1) {
+		return x;
+	} else {
+		return x * pow(x, n - 1);
 	}
-	return result;
 }
 
+console.log(pow(2, 1)); // 2
 console.log(pow(2, 2)); // 4
 console.log(pow(2, 3)); // 8
 console.log(pow(2, 4)); // 16
