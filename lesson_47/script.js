025f1a268e8eb5ff542a5f7f181de7f8e810138a
electra777 <electra0789@gmail.com>
2022-06-09 22:01:42 +0300

    add function getTotalProgressByRecursion

--- lesson_47/script.js
+++ lesson_47/script.js
@@ -86,3 +86,29 @@ function getTotalProgressByIteration(data) {
 }
 
 console.log(getTotalProgressByIteration(students));
+
+function getTotalProgressByRecursion(data) {
+	if (Array.isArray(data)) {
+		let total = 0;
+
+		for (let i = 0; i < data.length; i++) {
+			total += data[i].progress;
+		}
+
+		return [total, data.length];
+	} else {
+		let total = [0, 0];
+
+		for (let subData of Object.values(data)) {
+			const subDataArr = getTotalProgressByRecursion(subData);
+			total[0] += subDataArr[0];
+			total[1] += subDataArr[1];
+		}
+
+		return total;
+	}
+}
+
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0] / result[1]);
